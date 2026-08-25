8c84bb5672a6bf9250ae98d9195cc420674f5556
Amirmahmoud Rashidi <rashidi.amirmahmood@gmail.com>
2026-08-25 17:50:50 +0330

    Refactor circuit.py and add thevenin_norton method
    Refactor circuit.py to use wildcard import and fix indexing in build_matrices. Add thevenin_norton method for Thevenin and Norton equivalent calculations.

--- src/circuit_sim/circuit.py
+++ src/circuit_sim/circuit.py
@@ -1,5 +1,6 @@
-from src.circuit_sim.components import VoltageSource, Inductor
+from src.circuit_sim.components import *
 from src.circuit_sim.netlist_loader import NetlistLoader
+from src.circuit_sim.solver import solve_circuit
 
 class Circuit:
     def __init__(self,  string : str | None = None, filename = None , terminal : int | None = None):
@@ -28,14 +29,16 @@ class Circuit:
                 component.branch_index = index
                 index += 1
 
-        self.num_unknowns = index
+        return index
 
     def build_matrices(self):
         """Build the G_matrix and I_vector based on the circuit components."""
 
+        num_unknowns = self.assign_indices()
+
         # Initialize G_matrix and I_vector
-        G_matrix = [[0.0 for _ in range(self.num_unknowns)] for _ in range(self.num_unknowns)]
-        I_vector = [0.0 for _ in range(self.num_unknowns)]
+        G_matrix = [[0.0 for _ in range(num_unknowns)] for _ in range(num_unknowns)]
+        I_vector = [0.0 for _ in range(num_unknowns)]
 
         return G_matrix, I_vector
 
@@ -49,3 +52,45 @@ class Circuit:
             component.stamp(G_matrix, I_vector)
 
         return G_matrix, I_vector
+
+    def thevenin_norton(self, node1, node2):
+        index = [0, 0]
+        for node in self.nodes:
+            if node1 == node:
+                index[0] = node.index
+            if node2 == node:
+                index[1] = node.index
+        try:
+            G_matrix, I_vector = self.build_matrices
+
+            Y_matrix = solve_circuit(G_matrix, I_vector)
+
+            self.V_thevenin = Y_matrix[index[0]] - Y_matrix[index[1]]
+        except:
+            pass
+
+        try:
+
+            _temp_components = list(self.components)
+            V_test = VoltageSource("V_test", node1, node2, 0)
+            _temp_components.append(V_test)
+            G_matrix, I_vector = self.build_matrices
+
+            Y_matrix = solve_circuit(G_matrix, I_vector)
+
+            self.I_norton = Y_matrix[V_test.branch_index]
+        except:
+            pass
+
+        try:
+            _temp_components = list(self.components)
+            V_test = CurrentSource("I_test", node1, node2, 1)
+            _temp_components.append(V_test)
+            G_matrix, I_vector = self.build_matrices
+
+            Y_matrix = solve_circuit(G_matrix, I_vector)
+
+            self.V_thevenin = Y_matrix[index[0]] - Y_matrix[index[1]]
+
+        except:
+            pass
